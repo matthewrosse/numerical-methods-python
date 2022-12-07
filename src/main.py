79ce7d6538b87ab8d94d22c4738d1549c75c4997
Mateusz Różański <rozanski.mateusz00@gmail.com>
2022-12-07 20:13:03 +0100

    sarrus and laplace det

--- src/main.py
+++ src/main.py
@@ -20,11 +20,32 @@ def main():
     matrix_a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     matrix_b = [[2, 3, 4], [5, 7, 8], [9, 2, 3]]
 
+    matrix_5x5 = [
+        [123, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 23, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 27],
+    ]
+
     result = Matrix.multiply(matrix_a, matrix_b)
 
     print()
     print("matrix:")
     Matrix.print(result)
+
+    print("Det:")
+    print(Matrix.sarrus_det(matrix_b))
+
+    print()
+    print()
+    print()
+
+    Matrix.create_minor(matrix_b, 1, 1)
+
+    print("Laplace det")
+    print()
+    print(Matrix.laplace_det(matrix_5x5))
 
 
 if __name__ == "__main__":
--- src/matrix.py
+++ src/matrix.py
@@ -1,3 +1,7 @@
+import random as rd
+import math
+
+
 class Matrix:
     @staticmethod
     def multiply(a, b):
@@ -7,7 +11,8 @@ class Matrix:
         cols_b = len(b[0])
 
         if cols_a != rows_b:
-            raise Exception("Matrices cannot be multiplied, incorrect dimensions.")
+            raise Exception(
+                "Matrices cannot be multiplied, incorrect dimensions.")
 
         result = [[0 for row in range(cols_b)] for col in range(rows_a)]
 
@@ -19,8 +24,82 @@ class Matrix:
         return result
 
     @staticmethod
+    def sarrus_det_2x2(matrix):
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    @staticmethod
+    def sarrus_det_3x3(matrix):
+        # need to check dimensions and throw error
+        first = (
+            matrix[0][0] * matrix[1][1] * matrix[2][2]
+            + matrix[1][0] * matrix[2][1] * matrix[0][2]
+            + matrix[2][0] * matrix[0][1] * matrix[1][2]
+        )
+        second = (
+            matrix[1][0] * matrix[0][1] * matrix[2][2]
+            + matrix[0][0] * matrix[2][1] * matrix[1][2]
+            + matrix[2][0] * matrix[1][1] * matrix[0][2]
+        )
+
+        return first - second
+
+    @staticmethod
+    def create_minor(matrix, row, col):
+        result_rows = len(matrix) - 1
+        result_cols = len(matrix[0]) - 1
+
+        result = [[0 for row in range(result_cols)]
+                  for col in range(result_rows)]
+
+        for i in range(len(matrix)):
+            minor_row = i
+            if i > row:
+                minor_row -= 1
+
+            for j in range(len(matrix[0])):
+                minor_col = j
+                if j > col:
+                    minor_col -= 1
+
+                if i != row and j != col:
+                    result[minor_row][minor_col] = matrix[i][j]
+
+        return result
+
+    @staticmethod
+    def laplace_det(matrix):
+        if len(matrix) < 4:
+            return Matrix.sarrus_det(matrix)
+
+        rand_row = rd.randrange(0, len(matrix))
+
+        result = 0
+
+        for i in range(len(matrix[0])):
+            sub_matrix = Matrix.create_minor(matrix, rand_row, i)
+
+            result += (
+                matrix[rand_row][i]
+                * math.pow(-1, rand_row + i)
+                * Matrix.laplace_det(sub_matrix)
+            )
+
+        return result
+
+    @staticmethod
+    def sarrus_det(matrix):
+        if len(matrix) == 2 and len(matrix[0]) == 2:
+            return Matrix.sarrus_det_2x2(matrix)
+
+        if len(matrix) == 3 and len(matrix[0]) == 3:
+            return Matrix.sarrus_det_3x3(matrix)
+
+        raise Exception(
+            "Incorrect dimensions! You need to provide 2x2 or 3x3 matrix.")
+
+    @staticmethod
     def print(matrix) -> None:
         for row in matrix:
             for col in row:
-                print(f"{col:.2f}", end="")
+                print(f"{col:.2f}", end="\t")
             print()
